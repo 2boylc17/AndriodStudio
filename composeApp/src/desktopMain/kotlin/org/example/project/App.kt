6caefe2a76d581f6aba4eab9343bc47db9a5ed38
Callum Boylan <2boylc17@solent.ac.uk>
2024-11-14 16:42:12 +0000

    week 8

--- composeApp/src/desktopMain/kotlin/org/example/project/App.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/App.kt
@@ -6,12 +6,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+
 
 import week8project.composeapp.generated.resources.Res
 import week8project.composeapp.generated.resources.compose_multiplatform
@@ -20,18 +24,21 @@ import week8project.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        var studentlist = remember { mutableStateListOf<Student>() }
+        var newID = remember { mutableStateOf("") }
+        var newname = remember { mutableStateOf("") }
+        var newCourse = remember { mutableStateOf("") }
+
+        Column {
+            OutlinedTextField(value=newID.value, singleLine = true, onValueChange = { newID.value=it })
+            OutlinedTextField(value=newname.value, singleLine = true, onValueChange = { newname.value=it })
+            OutlinedTextField(value=newCourse.value, singleLine = true, onValueChange = { newCourse.value=it })
+            Button(onClick = {
+                var newStudent = Student(newID.toString(), newname.toString(), newCourse.toString())
+                studentlist.add(newStudent)
+            }) { Text("Add") }
         }
+
+
     }
 }
